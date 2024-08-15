af45add59f736a8314d8f66b62d9c4302abc8402
ritikayadav01 <144210251+ritikayadav01@users.noreply.github.com>
2024-08-16 00:02:34 +0530

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
-
+import userRouter from './routes/user.route.js'
 mongoose.connect("mongodb://localhost:27017").then(()=>{
     console.log("connected to monogodb!!");
 }).catch((err)=>{
@@ -10,3 +10,8 @@ mongoose.connect("mongodb://localhost:27017").then(()=>{
  app.listen(3000,()=>{
     console.log("Server is listening ")
  });
+
+//  creating api routes 
+// req: the data we get from the client side 
+// response : we send data from the server 
+app.use("/api/user",userRouter)
---
+++ api/routes/user.route.js
@@ -0,0 +1,5 @@
+import express from "express";
+import { test } from "../controllers/user.controller";
+const router =express.Router();
+router.get('/test',test);
+export default router ;
