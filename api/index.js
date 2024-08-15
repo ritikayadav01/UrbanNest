e93bbcbb2f17cb90d54640020c6bf469872446fe
ritikayadav01 <144210251+ritikayadav01@users.noreply.github.com>
2024-08-15 23:39:06 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,11 @@
 import express from "express";
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017").then(()=>{
+    console.log("connected to monogodb!!");
+}).catch((err)=>{
+    console.log(err);
+})
  const app =express();
  app.listen(3000,()=>{
     console.log("Server is listening ")
