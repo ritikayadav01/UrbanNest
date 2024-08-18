1735f9011cd82785eceb52b649fb669aba73bca4
ritikayadav01 <144210251+ritikayadav01@users.noreply.github.com>
2024-08-18 21:50:47 +0530

    create lisitng api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js"
 import dotenv from 'dotenv'
 import cookieParser from "cookie-parser";
 dotenv.config();
@@ -27,6 +28,8 @@ app.listen(3000, () => {
 // response : we send data from the server
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
+
 
 
 // creating a middleware 
